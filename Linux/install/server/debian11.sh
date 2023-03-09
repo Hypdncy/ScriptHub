9dc57cd71d7790579f0069072e9b2eca4081983d
Hypdncy <Hypdncy@outlook.com>
2023-03-09 15:08:31 +0800

    整理目录

--- Linux/install/server/debian11.sh
+++ Linux/install/server/debian11.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+my_dir=$(pwd)
 my_hostname="light"
-my_version_python="3.11.2"
-my_version_go="1.18.3"
+my_python_version="3.11.2"
+my_go_version="1.20.1"
 my_mysql_pass="Mysql1235.."
-
+my_mirrors_domain="mirrors.tencentyun.com"
+my_mirrors_url="http://${my_mirrors_domain}"
 apt-get -y install wget curl
 
 function install_apt() {
-  sed -i -E 's/(deb|security).debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list
+  sed -i -E "s/(deb|security).debian.org/${my_mirrors_domain}/g" /etc/apt/sources.list
 }
 
 function install_locale() {
@@ -21,11 +23,10 @@ function install_locale() {
 function install_vim() {
   apt-get -y purge vim*
   apt-get -y install vim
+  sed -i 's/filetype plugin indent on/filetype plugin indent off/g' /usr/share/vim/vim82/defaults.vim
 
   grep "^set nocompatible" /etc/vim/vimrc || echo "\
 set nocompatible
-set nosmartindent
-set noautoindent
 set expandtab
 set number
 set backspace=2
@@ -33,6 +34,11 @@ set mouse-=a
 set shiftwidth=4
 set tabstop=4
 set softtabstop=4
+
+set noautoindent
+set nocindent
+set nosmartindent
+set indentexpr=
 syntax on
 " >>/etc/vim/vimrc
   # 大小写控制
@@ -52,6 +58,8 @@ function install_dev() {
 }
 
 function install_ssh() {
+  cd "${my_dir}" || exit
+
   echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkKrEPadsD+82e8w3Epegm51FGELj6QyvsAn6V5Dt1WWifnhN9Cnk7TyTTmfziPY/DAgiA9vkYGua+jzQl+m7N0V5oMMXxiTSjEGJeHsVa4Qi+yK99CuLYv7T5FlamXTa80BUybc7xG66si2AWrYln2PHuuZuclwzxXeUHy73oaIheYnpxqy7bFnBJR6ojLoTJImnR/2RjntLRAZoR4w9eA25maEL00KXI5cP6mVopy6WRQuqCv27UjkMId5ivIKWEochtA2NiBGji6gKV9RBDRrlZ1O6FA1Y2DOUZxwPHLa708VCBzdwJRQ4dbvthcZ4YPj8uxZXOhTFKU3vsWrcZ0M2dXnPe3ioLNUGXiPd4Nlx38cBVnuTI44KVDOVxWFD0yqzGjDgb6Bdb6/5e/v2ypIwOQGyFiU7h3hXIKyi/13lA+3LAc94oOjT1mm//SwF4D6eDWda3PUGxptHo8eyocIc5NIR2dLs6ofhzllF29aTnw81jAgmEmaVtrSW1wns= hypdncy@Hypdncy-Mac.local" >~/.ssh/authorized_keys
   grep "PasswordAuthentication no" /etc/ssh/sshd_config || (
     sed -i 's@^#*PermitRootLogin prohibit-password$@PermitRootLogin yes@g' /etc/ssh/sshd_config
@@ -61,21 +69,8 @@ function install_ssh() {
   )
 }
 
-function install_bashrc() {
-  grep "^alias ls" ~/.bashrc || echo "\
-# PS1='\${debian_chroot:+(\$debian_chroot)}\\h:\\w\\\$ '
-PS1='\${debian_chroot:+(\$debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\\$ '
-umask 022
-export LS_OPTIONS='--color=auto'
-eval \"\$(dircolors)\"
-alias ls='ls \$LS_OPTIONS'
-alias ll='ls \$LS_OPTIONS -l'
-alias l='ls \$LS_OPTIONS -lA'
-" >>~/.bashrc
-
-}
-
 function install_proxychains() {
+  cd "${my_dir}" || exit
   # proxychains4
   apt-get -y purge proxychains*
   apt-get -y install proxychains4
@@ -87,30 +82,27 @@ function install_proxychains() {
 # Python
 
 function install_python() {
+  cd "${my_dir}" || exit
   apt-get -y install wget libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-  test -f ./Python-${my_version_python}.tar || wget https://repo.huaweicloud.com/python/${my_version_python}/Python-${my_version_python}.tar.xz
-  xz -d ./Python-${my_version_python}.tar.xz
-  tar -xvf ./Python-${my_version_python}.tar
-  cd Python-${my_version_python} || exit
-  ./configure --prefix=/usr/local/Python-${my_version_python} --enable-optimizations
+  test -f ./Python-${my_python_version}.tar ||
+    wget https://cdn.npmmirror.com/binaries/python/${my_python_version}/Python-${my_python_version}.tar.xz
+  xz -d ./Python-${my_python_version}.tar.xz
+  tar -xvf ./Python-${my_python_version}.tar
+  cd Python-${my_python_version} || exit
+  ./configure --prefix=/usr/local/Python-${my_python_version} --enable-optimizations
   make -j $(nproc) && make install
-  cd /opt || exit
-  /usr/local/Python-${my_version_python}/bin/python3 -m venv venv
-}
+  /usr/local/Python-${my_python_version}/bin/pip3 config set global.index-url https://${my_mirrors_domain}/pypi/simple
+  mkdir -p /opt/venv/
+  /usr/local/Python-${my_python_version}/bin/python3 -m venv /opt/venv/venv
 
-function install_pip() {
-  # pip 镜像源
-  test -f ~/.config/pip/pip.conf || (mkdir -p ~/.config/pip/ && echo "\
-[global]
-index-url = https://repo.huaweicloud.com/repository/pypi/simple
-trusted-host = repo.huaweicloud.com
-timeout = 120
-" >~/.config/pip/pip.conf)
+  echo -e "\nsource /opt/venv/venv/bin/activate\n" >~/.profile
 }
 
 function install_go() {
-  test -f go${my_version_go}.linux-amd64.tar.gz || wget https://mirrors.aliyun.com/golang/go${my_version_go}.linux-amd64.tar.gz
-  rm -rf /usr/local/go && tar -C /usr/local -xzf go${my_version_go}.linux-amd64.tar.gz
+  cd "${my_dir}" || exit
+
+  test -f ./go${my_go_version}.linux-amd64.tar.gz || wget https://dl.google.com/go/go${my_go_version}.linux-amd64.tar.gz
+  rm -rf /usr/local/go && tar -C /usr/local -xzf go${my_go_version}.linux-amd64.tar.gz
 
   grep "/usr/local/go/bin" ~/.profile || {
     echo "export PATH=\$PATH:/usr/local/go/bin"
@@ -122,11 +114,15 @@ function install_go() {
 }
 
 function install_mysql() {
+  cd "${my_dir}" || exit
+
   # 安装
+  wget https://repo.mysql.com//mysql-apt-config_0.8.24-1_all.deb
+  apt install ./mysql-apt-config_0.8.24-1_all.deb -y
   apt-get update
+
   apt-get install mysql-server
-  apt-get install mysql-client
-  systemctl restart mysqld
+  systemctl restart mysql
   mysql -uroot -p --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${my_mysql_pass}';FLUSH PRIVILEGES;"
   mysql -uroot -p"${my_mysql_pass}" --connect-expired-password -e "UPDATE mysql.user SET Host = '%' WHERE User = 'root';FLUSH PRIVILEGES;"
 
@@ -135,18 +131,20 @@ function install_mysql() {
 }
 
 function install_docker() {
+  cd "${my_dir}" || exit
+
   # step 1: 安装必要的一些系统工具
   apt-get purge -y docker docker-engine docker.io
   apt-get update
   apt-get -y install apt-transport-https ca-certificates curl software-properties-common
   # step 2: 安装GPG证书
-  curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/debian/gpg | apt-key add -
+  curl -fsSL ${my_mirrors_url}/docker-ce/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
   # Step 3: 写入软件源信息
-  add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/debian $(lsb_release -cs) stable"
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] ${my_mirrors_url}/docker-ce/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
+
   # Step 4: 更新并安装Docker-CE
   apt-get -y update
   apt-get -y install docker-ce
-
 }
 
 #install_apt
@@ -157,7 +155,5 @@ install_ssh
 install_bashrc
 install_proxychains
 install_python
-install_pip
 install_go
 install_docker
-install_mysql
